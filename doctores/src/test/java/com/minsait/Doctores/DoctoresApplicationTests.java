ad09fd75f62c731b10c935785e8c4e36d0f09996
Zaldivar Ramirez <euzaldivar@minsait.com>
2023-10-16 18:54:58 -0600

    Sexto commit subiendo microservicio doctores

---
+++ doctores/src/test/java/com/minsait/Doctores/DoctoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minsait.Doctores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
